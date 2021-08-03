50466d4bb03c8d14fe951bed5841145260fe4818
Elizabeth Mejía <84818406+Elyy02@users.noreply.github.com>
2021-08-02 22:31:59 -0500

    Funcionalidad Javascrip

---
+++ scrips.js
@@ -0,0 +1,5 @@
+mostrartabla();
+let fecha = document.getElementById("fecha");
+let tema = document.getElementById("tema");
+let link = document.getElementById("link");
+let agregarbtn = document.getElementById("agregarbtn");
